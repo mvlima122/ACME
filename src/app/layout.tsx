c778582fa611eaa6ec328584edadf57af27d14c5
mvlima122 <vivilima71@gmail.com>
2026-02-28 15:23:00 +0000

    feat: implementar estrutura base raiz

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type {Metadata} from 'next';
+import { inter } from './ui/fonts';
+import React from 'react';
+
+export const metadata:Metadata = {
+  title: 'ACME'
+}
+
+export default function RootLayout({children}:{children:React.ReactNode})
+{
+  return (
+    <html lang= 'pt-BR'>
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
