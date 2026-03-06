0cc55bdc01d31c50784675a18f3b1f80293f67ac
mvlima122 <vivilima71@gmail.com>
2026-03-06 01:42:25 +0000

    chore: configurar framework css

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './scr/app/**/*.(.js,.ts,.jsx,.tsx)',
+    './scr/components/**/*.(.js,.ts,.jsx,.tsx)'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+
+};
+
+export default config;
